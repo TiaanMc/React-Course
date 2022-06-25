3b4224aaad93b75f13692420eb1d14f61d142425
TiaanMc <104506703+TiaanMc@users.noreply.github.com>
2022-06-25 13:03:02 +0200

    React Course Day 2
    Learning styling in react and events. Functional components

---
+++ src/Content.js
@@ -0,0 +1,32 @@
+const Content = () => {
+    const handleNameChange = () => {
+        const names = ["bob", "kevin", "tiaan"];
+        const int = Math.floor(Math.random() * 3);
+        return names[int];
+      }
+
+      const handleClick = () => {
+        console.log('You clicked it')
+      }
+
+      const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+      }
+
+      const handleClick3 = (e) => {
+        console.log(e.target)
+      }
+
+    return (
+        <main>
+            <p onDoubleClick={handleClick}>
+                Hello {handleNameChange()}
+            </p>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => handleClick2('Tiaan')}>Click it</button>
+            <button onClick={(e) => handleClick3(e)}>Click it</button>
+        </main>
+    )
+}
+
+export default Content
